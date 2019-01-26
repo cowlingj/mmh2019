11d2176a1900ee9bc04e700a97e148b01691754c
SirWaffleOfSyrup <faganj-y11@hotmai.co.uk>
2019-01-26 19:55:40 +0000

    Star Map Behaviour

---
+++ Assets/StarMap/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Move is called on Ship Cursor move
+    public static void Move(float horizontal, float vertical, GameObject ship)
+    {
+        Vector3 currentv = ship.transform.position;
+        currentv.x = currentv.x + horizontal;
+        currentv.y = currentv.y + vertical;
+        ship.transform.TransformVector(currentv);
+    }
+
+}
---
+++ Assets/StarMap/ShipBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Behaviour : MonoBehaviour
+{
+
+    public GameObject ship; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = CrossPlatformInputManager.GetAxis("Veritcal");
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        bool returnkey = CrossPlatformInputManager.GetButton("return");
+
+        if (returnkey)
+        {
+            //GotoPlanet
+        }
+        else
+        {
+            Movement.Move(horizontal, vertical, ship);
+        }
+    }
+}
