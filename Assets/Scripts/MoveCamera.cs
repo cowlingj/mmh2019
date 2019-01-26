8c40a5a66d29fb47bcf58589ddd260b6f4e777ce
Jonathan Cowling <09cowlingj@gmail.com>
2019-01-26 15:11:41 +0000

    Add Bridge and camera script

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public GameObject target;
+    public Vector3 cameraOffset;
+
+    // Update is called once per frame
+      void Update() {
+          transform.position = target.transform.position + cameraOffset;
+      }
+}
