dbbd5f02cd421f197a518f281bc3af0b1344bb20
Jonathan Cowling <09cowlingj@gmail.com>
2019-01-27 11:14:09 +0000

    Updated StarMap scene

---
+++ Assets/PlanetTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlanetTrigger : MonoBehaviour
+{
+
+    private bool insidePlanet = false;
+    private Trigger trigger;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        trigger = new Trigger(()=>{
+            Debug.Log("collision detected");
+        }, ()=>{
+            return insidePlanet
+              && CrossPlatformInputManager.GetButton("Submit");
+            });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trigger.TestTrigger();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        insidePlanet = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        insidePlanet = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        insidePlanet = false;
+    }
+}
