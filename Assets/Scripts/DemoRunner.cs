95467d44b3f261e3fe66f5392c56ad14e8174a52
Jonathan Cowling <09cowlingj@gmail.com>
2019-06-21 22:24:39 +0100

    No longer working on this project, commit leftovers

---
+++ Assets/Scripts/DemoRunner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DemoRunner : MonoBehaviour
+{
+    public GameObject menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool firePressed = CrossPlatformInputManager.GetButton("Cancel");
+        menu.SetActive(firePressed);
+    }
+
+    public void LoadLevel(string name) {
+        Debug.Log(name);
+        if (name.Equals("")) {
+            Application.Quit();
+            return;
+        }
+        SceneManager.LoadScene(name);
+    }
+}
